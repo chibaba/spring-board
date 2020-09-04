ff918178fdacdbc3fbc21332bc492283084d1e9a
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-04 01:20:12 +0100

    first commit

---
+++ src/main/java/nedstack/projectboard/domain/ProjectTask.java
@@ -0,0 +1,17 @@
+package nedstack.projectboard.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ProjectTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String summary;
+    private String acceptanceCriteria
+}
