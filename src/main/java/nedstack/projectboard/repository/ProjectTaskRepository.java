0ad1de7b67fcffcd8acabbd8ddde6a780398c6ce
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-04 02:16:59 +0100

    wip

---
+++ src/main/java/nedstack/projectboard/repository/ProjectTaskRepository.java
@@ -0,0 +1,8 @@
+package nedstack.projectboard.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository {
+}
