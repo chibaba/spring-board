0cab861aa9ccb187a1769ee804884de59541147d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-04 01:26:03 +0100

    wip

--- src/main/java/nedstack/projectboard/domain/ProjectTask.java
+++ src/main/java/nedstack/projectboard/domain/ProjectTask.java
@@ -13,5 +13,42 @@ public class ProjectTask {
     private Long id;
 
     private String summary;
-    private String acceptanceCriteria
+    private String acceptanceCriteria;
+    private  String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAcceptanceCriteria() {
+        return acceptanceCriteria;
+    }
+
+    public void setAcceptanceCriteria(String acceptanceCriteria) {
+        this.acceptanceCriteria = acceptanceCriteria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ProjectTask() {
+
+    }
 }
